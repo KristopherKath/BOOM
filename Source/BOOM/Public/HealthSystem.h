360a87aecc784e3b9b5c89a1f849539c39c10d43
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-01-01 22:14:46 -0800

    added health component. combine blueprint and c++, will explain later

---
+++ Source/BOOM/Public/HealthSystem.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BOOM_API HealthSystem
+{
+private:
+	float Health = 100.f;
+public:
+	HealthSystem();
+	~HealthSystem();
+	
+
+};
