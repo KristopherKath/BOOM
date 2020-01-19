6867d5ac66f39df69322fe495d32b145783e5793
Chloe Culver <c.culver831@gmail.com>
2020-01-18 16:45:42 -0800

    Weapon Socket @Hanson

--- Source/BOOM/Private/ChooseNextWaypoint.cpp
+++ Source/BOOM/Private/ChooseNextWaypoint.cpp
@@ -24,7 +24,7 @@ EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& Own
 	auto NextIndex = (Index + 1) % PatrolPoints.Num();
 	//Index = NextIndex;
 	BlackBordComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
-	UE_LOG(LogTemp, Warning, TEXT("Waypoint index : %i"),Index);
+	//UE_LOG(LogTemp, Warning, TEXT("Waypoint index : %i"),Index);
 
 	//Node equivalent of task succeeded
 	//must have return statement in every C++ class
