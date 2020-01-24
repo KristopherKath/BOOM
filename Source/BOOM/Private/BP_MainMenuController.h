008cf0f78340f702b46498d68428c84c98f351d7
nguyenhanson1 <nguyenhanson1@gmail.com>
2020-01-23 22:31:43 -0800

    main menu controller WIP

---
+++ Source/BOOM/Private/BP_MainMenuController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BP_MainMenuController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABP_MainMenuController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
